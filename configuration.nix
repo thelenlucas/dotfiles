1b765a22d5cb1661f101171d01b0d143bb404c66
Lucas Thelen <thelenlucas028@gmail.com>
2025-07-18 19:54:13 -0600

    bluetooth

--- configuration.nix
+++ configuration.nix
@@ -33,11 +33,48 @@
 
   programs.steam.enable = true;
 
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+
+  services.blueman.enable = true;
+
+  # Postgres
+  services.postgresql = {
+    enable = true;
+    ensureDatabases = [ "mydatabase" ];
+
+    identMap = ''
+      # ArbitraryMapName systemUser DBUser
+         superuser_map      root      postgres
+         superuser_map      postgres  postgres
+         # Let other names login as themselves
+         superuser_map      /^(.*)$   \1
+    '';
+
+    enableTCPIP = true;
+    # port = 5432;
+    authentication = pkgs.lib.mkOverride 10 ''
+      #...
+      #type database DBuser origin-address auth-method
+      local all       all     trust
+      # ipv4
+      host  all      all     127.0.0.1/32   trust
+      # ipv6
+      host all       all     ::1/128        trust
+    '';
+    initialScript = pkgs.writeText "backend-initScript" ''
+      CREATE ROLE nixcloud WITH LOGIN PASSWORD 'nixcloud' CREATEDB;
+      CREATE DATABASE nixcloud;
+      GRANT ALL PRIVILEGES ON DATABASE nixcloud TO nixcloud;
+    '';
+  };
+
   # System packages
   environment.systemPackages = with pkgs; [
     grim
     slurp
     wl-clipboard
+    bluetui
     mako
     kdePackages.dolphin
     firefox
@@ -155,7 +192,7 @@
   };
 
   # Set your time zone.
-  time.timeZone = "America/Chicago";
+  time.timeZone = "America/Denver";
 
   users.users.lucas = {
     isNormalUser = true;
