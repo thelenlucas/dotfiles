d39922b276331e88895040faae7aeb88aca85306
Lucas Thelen <thelenlucas028@gmail.com>
2025-07-20 07:38:45 -0600

    Nix-topology setup

--- flake.nix
+++ flake.nix
@@ -5,46 +5,63 @@
     nixpkgs.url = "github:NixOs/nixpkgs/release-25.05";
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     flake-utils.url = "github:numtide/flake-utils";
+    nix-topology.url = "github:oddlama/nix-topology";
+    nix-topology.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, flake-utils }: {
-    nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./nixos/configuration.nix
-        ./hardware/laptop.nix
+  outputs = inputs@{ self, nixpkgs, home-manager, flake-utils, nix-topology }:
+    {
+      nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./nixos/configuration.nix
+          ./hardware/laptop.nix
+          nix-topology.nixosModules.default
 
-        { networking.hostName = "laptop"; }
+          { networking.hostName = "laptop"; }
 
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.lucas = ./home/home.nix;
-        }
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.lucas = ./home/home.nix;
+          }
 
-      ];
-    };
+        ];
+      };
 
-    nixosConfigurations.labtop = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./nixos/configuration.nix
-        ./hardware/labtop.nix
-        ./nixos/server_power.nix
-        ./nixos/k3s.nix
+      nixosConfigurations.labtop = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./nixos/configuration.nix
+          ./hardware/labtop.nix
+          ./nixos/server_power.nix
+          ./nixos/k3s.nix
+          nix-topology.nixosModules.default
 
-        { networking.hostName = "labtop"; }
+          { networking.hostName = "labtop"; }
 
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.lucas = ./home/home.nix;
-        }
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.lucas = ./home/home.nix;
+          }
 
-      ];
-    };
-  };
+        ];
+      };
+    } // flake-utils.lib.eachDefaultSystem (system: rec {
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ nix-topology.overlays.default ];
+      };
 
+      topology = import nix-topology {
+        inherit pkgs;
+        modules = [
+          ./topology.nix
+          { nixosConfigurations = self.nixosConfigurations; }
+        ];
+      };
+    });
 }
